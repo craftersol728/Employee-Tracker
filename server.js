0cc2ba25b0e6f6a23913097ff0a661f94065d88e
Jerry Wan <craftersol728@gmail.com>
2023-01-22 19:15:13 -0800

    added console.table

---
+++ server.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password123',
+    database: 'company_db',
+  });
