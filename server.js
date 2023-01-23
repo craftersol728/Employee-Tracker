1ae75a77902fd4c1792a5ccbd5ca14f2ec29ff83
Jerry Wan <craftersol728@gmail.com>
2023-01-22 19:33:01 -0800

    added the rest of the departments and input with data update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const connection = mysql.createConnection({
 
   const startMenu = () => {
     inquirer.prompt({
-        message: 'Good morning. Its 7 AM The weather in Malibu is 72 degrees with scattered clouds. The surf conditions are fair with waist to shoulder highlines, high tide will be at 10:52 a.m  What would you like to do today',
+        message: 'Good morning. Its 7 AM The weather in Malibu is 72 degrees with scattered clouds. The surf conditions are fair with waist to shoulder highlines, high tide will be at 10:52 a.m  Jarvis At your service,',
         name: 'menu',
         type: 'list',
         choices: [ 
@@ -93,3 +93,118 @@ const viewEmployees = () => {
     );
   };
   
+
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+          name: 'department',
+          type: 'input',
+          message: 'How many ounces a day of this gobbledygook am I supposed to drink? What is the department name?',
+        },
+      ])
+      .then(answer => {
+        connection.query(
+          'INSERT INTO department (dept_name) VALUES (?)',
+          [answer.department],
+          function (err, res) {
+            if (err) throw err;
+            console.log('Sir, the more you struggle, the more this is going to hurt. Department added');
+            startMenu();
+          }
+        );
+      });
+  };
+
+  const addJob = () => {
+    inquirer.prompt([
+        {
+          name: 'jobTitle',
+          type: 'input',
+          message: 'Oh, hello sir.What is the job title?',
+        },
+        {
+          name: 'salary',
+          type: 'input',
+          message: 'The armor is now at 92% What is the salary for this job?',
+        },
+        {
+          name: 'deptId',
+          type: 'input',
+          message: 'Uh, say J.A.R.V.I.S., is it that time? What is the department ID number?',
+        },
+      ])
+      .then(answer => {
+        connection.query(
+          'INSERT INTO job (title, salary, department_id) VALUES (?, ?, ?)',
+          [answer.jobTitle, answer.salary, answer.deptId],
+          function (err, res) {
+            if (err) throw err;
+            console.log('Job added!');
+            startMenu();
+          }
+        );
+      });
+  };
+
+  const addEmployee = () => {
+    inquirer.prompt([
+        {
+          name: 'nameFirst',
+          type: 'input',
+          message: "I'm sorry, I was asleep. Or... I was a dream? What is the employee's first name?",
+        },
+        {
+          name: 'nameLast',
+          type: 'input',
+          message: "Reboot. Legionnaire 06's got a buggy suit. What is the employee's last name?",
+        },
+        {
+          name: 'jobId',
+          type: 'input',
+          message: "Wouldn't've been my first call. But, down in the real world, we're faced with ugly choices What is the employee's job id?",
+        },
+        {
+          name: 'managerId',
+          type: 'input',
+          message: 'Im not Ultron. Im not J.A.R.V.I.S.. I am... I am... What is the manager Id?',
+        },
+      ])
+      .then(answer => {
+        connection.query(
+          'INSERT INTO employee (first_name, last_name, job_id, manager_id) VALUES (?, ?, ?, ?)',
+          [answer.nameFirst, answer.nameLast, answer.jobId, answer.managerId],
+          function (err, res) {
+            if (err) throw err;
+            console.log('Employee added!');
+            startMenu();
+          }
+        );
+      });
+  };
+  
+  const updateEmployee = () => {
+    inquirer
+      .prompt([
+        {
+          name: 'id',
+          type: 'input',
+          message: 'Ill continue to run variations on the interface, but you should probably prepare for your guests. Ill notify you if there are any developments. Enter employee id',
+        },
+        {
+          name: 'jobId',
+          type: 'input',
+          message: 'I am a program. I am without form. Enter new job id',
+        },
+      ])
+      .then(answer => {
+        connection.query(
+          'UPDATE employee SET job_id=? WHERE id=?',
+          [answer.jobId, answer.id],
+          function (err, res) {
+            if (err) throw err;
+            console.log('Its too much...making me... Oh. No. Employee updated!');
+            startMenu();
+          }
+        );
+      });
+  };
